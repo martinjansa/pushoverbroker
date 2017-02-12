d0a2598f6b4251448bf55caf657a676790c12c38
Martin Jansa <martin.jansa@seznam.cz>
2017-02-12 20:48:21 +0100

    Draft of a failing acceptance tetst for the REST API 1/message POST request

---
+++ tests/PushoverBrokerRESTAPI_Test/PushoverBrokerRESTAPI_test.go
@@ -0,0 +1,10 @@
+package PushoverBrokerRESTAPITest
+
+import (
+	"testing"
+)
+
+// TestAPIShouldAcceptPOST1MessagesJsonWithEmptyMessage is a test function for the REST API call
+func TestAPIShouldAcceptPOST1MessagesJsonWithEmptyMessage(t *testing.T) {
+	t.Error("The acceptance test has not been implemented.")
+}
