ce3e86fe6dcbd935d99be89cb16cd8ab776db243
Martin Jansa <martin.jansa@seznam.cz>
2017-02-14 23:51:48 +0100

    Finished REST API server acceptance test implementation a dummy json.

--- pushoverbroker_test.go
+++ pushoverbroker_test.go
@@ -1,7 +1,9 @@
 package pushoverbroker_test
 
 import (
-	"strconv"
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
 	"github.com/martinjansa/pushoverbroker/server"
@@ -37,10 +39,38 @@ func (mh *MessageHandlerMock) AssertMessageAcceptedOnce(t *testing.T, message se
 
 // TestAPIShouldAcceptPOST1MessagesJsonWithEmptyMessage is a test function for the REST API call
 func TestAPIShouldAcceptPOST1MessagesJsonWithEmptyMessage(t *testing.T) {
+
+	// GIVEN REST API server is initialized and connected to the message handler mock
 	messageHandlerMock := NewMessageHandlerMock()
 	brokerServer := server.NewServer(messageHandlerMock)
 	testRequest := server.Message{Request: "test request"}
-	port := 8500
-	go brokerServer.Martini.RunOnAddr(":" + strconv.Itoa(port))
+	portStr := "8500"
+	go brokerServer.Martini.RunOnAddr(":" + portStr)
+
+	// WHEN a json POST request is sent via the REST API
+	url := "http://localhost:" + portStr + "/1/messages.json"
+	var jsonStr = []byte(testRequest.GetRequest())
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	//req.Header.Set("X-Custom-Header", "myvalue")
+	//req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("POST request failed with error %s.", err)
+	}
+	defer resp.Body.Close()
+
+	// CHECK
+
+	// the request should respond correctly
+	if resp.StatusCode != 200 {
+		t.Errorf("POST request returned status code %d and status message %s. Expected code 200.", resp.StatusCode, resp.Status)
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	t.Logf("POST request response body '%s'.", string(body))
+
+	// the right message shoud be delivered to the mock
 	messageHandlerMock.AssertMessageAcceptedOnce(t, testRequest)
 }
