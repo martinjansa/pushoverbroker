a4625ea72ff96020abc6159bdb4bc574ef76ba5e
Martin Jansa <martin.jansa@seznam.cz>
2017-02-18 15:02:05 +0100

    Renamed PushoverConnector interface to PushNotificationsSender

--- pushoverconnector.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// PushoverConnector represents the connector to the Pushover API
-type PushoverConnector interface {
-	PostPushNotificationMessage(message PushNotification) error
-}
