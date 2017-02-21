09966cd185418161ab45ca3fca8f474ee8934b7b
Martin Jansa <martin.jansa@seznam.cz>
2017-02-21 23:35:29 +0100

    implemented checks that processor propagates success or permanent errors and convert temporary errors to 202 (Accepted)

--- processor_test.go
+++ processor_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 // TestShouldSendMessageToPushNotificationsSender tests whether the processor attempts to send all the incomming messages to Pushover connector
 func TestShouldSendMessageToPushNotificationsSender(t *testing.T) {
@@ -30,4 +33,116 @@ func TestShouldSendMessageToPushNotificationsSender(t *testing.T) {
 
 	// the right message shoud be delivered to the mock
 	pcm.AssertMessageAcceptedOnce(t, testMessage)
+}
+
+// TestShouldPropagateSuccessOrPermanentFailureResponses tests whether the processor returns the right response codes from the external service calls
+func TestShouldPropagateSuccessOrPermanentFailureResponses(t *testing.T) {
+
+	var testcases = []struct {
+		id                 string
+		responseStatusCode int
+	}{
+		{"ShouldPropagateSuccess200", 200},
+		{"ShouldPropagateError400", 400},
+		{"ShouldPropagateError401", 401},
+		{"ShouldPropagateError402", 402},
+		{"ShouldPropagateError403", 403},
+		{"ShouldPropagateError404", 404},
+		{"ShouldPropagateError405", 405},
+		{"ShouldPropagateError426", 426},
+		{"ShouldPropagateError500", 500},
+	}
+	// **** GIVEN ****
+
+	// The REST API server is initialized and connected to the message handler mock
+	pcm := NewPushNotificationsSenderMock()
+	processor := NewProcessor(pcm)
+
+	testMessage := PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}
+
+	// start the processor
+	go processor.Run()
+
+	for _, tc := range testcases {
+
+		t.Run(tc.id, func(t *testing.T) {
+
+			// **** WHEN ****
+
+			// set the response in the mock
+			pcm.ForceResponse(nil, tc.responseStatusCode)
+
+			// a push notification is obtained by the process (via IncommingPushNotificationMessageHandler interface method HandleMessage())
+			err, responseCode := processor.HandleMessage(testMessage)
+
+			// **** THEN ****
+
+			// the request should respond correctly
+			if err == nil {
+
+				// check the reseponse code
+				if responseCode != tc.responseStatusCode {
+					t.Errorf("The handling of the message returned response code %d, but expected was %d", responseCode, tc.responseStatusCode)
+				}
+
+			} else {
+
+				t.Errorf("Handling of the message failed with error %s, response code %d.", err, responseCode)
+			}
+		})
+	}
+}
+
+// TestShouldReturnAcceptedOnTemporaryFailure tests whether the processor returns the HTTP error 202 (Accepted) if the call to the external service fails with a temporary failure
+func TestShouldReturn202AcceptedOnTemporaryFailure(t *testing.T) {
+
+	var testcases = []struct {
+		id                 string
+		responseError      error
+		responseStatusCode int
+	}{
+		{"ShouldReturn202OnPostError", errors.New("post error"), 0},
+		{"ShouldReturn202OnGatewayTimeOut504", nil, 504},
+		{"ShouldReturn202OnNetworkReadTimeOut598", nil, 598},
+		{"ShouldReturn202OnNetworkTimeOut599", nil, 599},
+	}
+	// **** GIVEN ****
+
+	// The REST API server is initialized and connected to the message handler mock
+	pcm := NewPushNotificationsSenderMock()
+	processor := NewProcessor(pcm)
+
+	testMessage := PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}
+
+	// start the processor
+	go processor.Run()
+
+	for _, tc := range testcases {
+
+		t.Run(tc.id, func(t *testing.T) {
+
+			// **** WHEN ****
+
+			// set the response in the mock
+			pcm.ForceResponse(tc.responseError, tc.responseStatusCode)
+
+			// a push notification is obtained by the process (via IncommingPushNotificationMessageHandler interface method HandleMessage())
+			err, responseCode := processor.HandleMessage(testMessage)
+
+			// **** THEN ****
+
+			// the request should respond correctly
+			if err == nil {
+
+				// check the response code
+				if responseCode != 202 {
+					t.Errorf("The handling of the message returned response code %d, but expected was 202", responseCode)
+				}
+
+			} else {
+
+				t.Errorf("Handling of the message failed with error %s, response code %d.", err, responseCode)
+			}
+		})
+	}
 }
