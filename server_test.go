31ffeaaf3c1037d8012c59780c560bfa202bd2e1
Martin Jansa <martin.jansa@seznam.cz>
2017-02-21 22:59:07 +0100

    refactored test cases (eliminated duplicate code, speedup by common init part), implemented several error handling tests for the server

--- server_test.go
+++ server_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"crypto/tls"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -14,6 +15,8 @@ import (
 
 // implements the IncommingPushNotificationMessageHandler interface
 type MessageHandlerMock struct {
+	responseErr         error
+	responseCode        int
 	handleMessageCalled int
 	notification        PushNotification
 }
@@ -21,6 +24,8 @@ type MessageHandlerMock struct {
 // NewMessageHandlerMock initializes the mock
 func NewMessageHandlerMock() *MessageHandlerMock {
 	mh := new(MessageHandlerMock)
+	mh.responseErr = nil
+	mh.responseCode = 200
 	mh.handleMessageCalled = 0
 	return mh
 }
@@ -28,7 +33,13 @@ func NewMessageHandlerMock() *MessageHandlerMock {
 func (mh *MessageHandlerMock) HandleMessage(message PushNotification) (error, int) {
 	mh.handleMessageCalled++
 	mh.notification = message
-	return nil, 200
+	return mh.responseErr, mh.responseCode
+}
+
+func (mh *MessageHandlerMock) ForceResponse(responseErr error, reseponseCode int) {
+	mh.handleMessageCalled = 0
+	mh.responseErr = responseErr
+	mh.responseCode = reseponseCode
 }
 
 func (mh *MessageHandlerMock) AssertMessageAcceptedOnce(t *testing.T, message PushNotification) {
@@ -40,8 +51,60 @@ func (mh *MessageHandlerMock) AssertMessageAcceptedOnce(t *testing.T, message Pu
 	}
 }
 
-// ImplTestServerShouldAcceptTLSPOST1MessagesJSON is a test function for the REST API call
-func ImplTestServerShouldAcceptTLSPOST1MessagesJSON(t *testing.T, port int, message string) {
+// TestServerAPI1MessageJSONShouldForwardToMessageHandler is a test function for the REST API call
+func TestServerAPI1MessageJSONShouldForwardToMessageHandler(t *testing.T) {
+
+	var testcases = []struct {
+		id                 string
+		urlValues          map[string]string
+		responseErr        error
+		responseStatusCode int
+		expectedMessage    PushNotification
+		expectedStatusCode int
+	}{
+		{
+			// checks that the success result is propagated if a call to push notification sender succeeds
+			"ShouldReturnSuccessForEmptyMessage",
+			map[string]string{"token": "<dummy token>", "user": "<dummy user>", "message": ""}, nil, 200,
+			PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}, 200,
+		},
+		{
+			// checks that the success result is propagated if a call to push notification sender succeeds
+			"ShouldReturnSuccessForNonEmptyMessage",
+			map[string]string{"token": "<dummy token>", "user": "<dummy user>", "message": "<dummy message>"}, nil, 200,
+			PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: "<dummy message>"}, 200,
+		},
+		{
+			// checks that the success result 202 Accepted is returned if this code is passed from the message handler
+			"ShouldReturn202FromNotificationHandler",
+			map[string]string{"token": "<dummy token>", "user": "<dummy user>", "message": ""}, nil, 202,
+			PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}, 202,
+		},
+		{
+			// checks that the error code 403 is returned if handling in the notification handler fails returns this status code
+			"ShouldReturn403FromNotificationHandler",
+			map[string]string{"token": "<dummy token>", "user": "<dummy user>", "message": ""}, nil, 403,
+			PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}, 403,
+		},
+		{
+			// checks that the error code 429 is returned if handling in the notification handler fails returns this status code
+			"ShouldReturn429FromNotificationHandler",
+			map[string]string{"token": "<dummy token>", "user": "<dummy user>", "message": ""}, nil, 429,
+			PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}, 429,
+		},
+		{
+			// checks that the error code 500 is returned if handling in the notification handler fails returns this status code
+			"ShouldReturn500FromNotificationHandler",
+			map[string]string{"token": "<dummy token>", "user": "<dummy user>", "message": ""}, nil, 500,
+			PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}, 500,
+		},
+		{
+			// checks that the internal server error 500 is returned if handling in the notification handler fails
+			"ShouldReturn500OnInternalError",
+			map[string]string{"token": "<dummy token>", "user": "<dummy user>", "message": ""}, errors.New("any internal error"), 0,
+			PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: ""}, 500,
+		},
+	}
 
 	// **** GIVEN ****
 
@@ -51,61 +114,56 @@ func ImplTestServerShouldAcceptTLSPOST1MessagesJSON(t *testing.T, port int, mess
 
 	// The REST API server is initialized and connected to the message handler mock
 	messageHandlerMock := NewMessageHandlerMock()
+	port := 8502
 	brokerServer := NewServer(port, certFilePath, keyFilePath, messageHandlerMock)
 	go brokerServer.Run()
 
-	// **** WHEN ****
+	for _, tc := range testcases {
 
-	//a json POST request is sent via the REST API
-	expectedMessage := PushNotification{Token: "<dummy token>", User: "<dummy user>", Message: message}
+		t.Run(tc.id, func(t *testing.T) {
 
-	// encode message into the URL form values
-	form := url.Values{}
-	form.Set("token", "<dummy token>")
-	form.Set("user", "<dummy user>")
-	form.Set("message", message)
-	formStr := form.Encode()
+			// **** WHEN ****
 
-	// Prepare the POST request with form data
-	urlStr := "https://localhost:" + strconv.Itoa(port) + "/1/messages.json"
-	req, err := http.NewRequest("POST", urlStr, bytes.NewBufferString(formStr))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Content-Length", strconv.Itoa(len(formStr)))
+			// encode message into the URL form values
+			form := url.Values{}
+			for name, value := range tc.urlValues {
+				form.Set(name, value)
+			}
+			formStr := form.Encode()
 
-	// initialize the client that does not check the certificates (for testing purposes only)
-	tlsConfig := tls.Config{InsecureSkipVerify: true}
-	transport := &http.Transport{TLSClientConfig: &tlsConfig}
-	client := &http.Client{Transport: transport}
+			// Prepare the POST request with form data
+			urlStr := "https://localhost:" + strconv.Itoa(port) + "/1/messages.json"
+			req, err := http.NewRequest("POST", urlStr, bytes.NewBufferString(formStr))
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			req.Header.Add("Content-Length", strconv.Itoa(len(formStr)))
 
-	// post the request
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Errorf("POST request failed with error %s.", err)
-		return
+			// initialize the client that does not check the certificates (for testing purposes only)
+			tlsConfig := tls.Config{InsecureSkipVerify: true}
+			transport := &http.Transport{TLSClientConfig: &tlsConfig}
+			client := &http.Client{Transport: transport}
+
+			// force the moct to fail the posting of the push notification message
+			messageHandlerMock.ForceResponse(tc.responseErr, tc.responseStatusCode)
+
+			// post the request
+			resp, err := client.Do(req)
+			if err != nil {
+				t.Errorf("POST request failed with error %s, but was expected to succeed.", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			// **** THEN ****
+
+			// check the expected response code
+			if resp.StatusCode != tc.expectedStatusCode {
+				t.Errorf("POST request returned status code %d and status message %s. Expected code %d.", resp.StatusCode, resp.Status, tc.expectedStatusCode)
+			}
+			body, _ := ioutil.ReadAll(resp.Body)
+			t.Logf("POST request response body '%s'.", string(body))
+
+			// the right message shoud be delivered to the mock
+			messageHandlerMock.AssertMessageAcceptedOnce(t, tc.expectedMessage)
+		})
 	}
-	defer resp.Body.Close()
-
-	// **** THEN ****
-
-	// the request should respond correctly
-	if resp.StatusCode != 200 {
-		t.Errorf("POST request returned status code %d and status message %s. Expected code 200.", resp.StatusCode, resp.Status)
-	}
-	body, _ := ioutil.ReadAll(resp.Body)
-	t.Logf("POST request response body '%s'.", string(body))
-
-	// the right message shoud be delivered to the mock
-	messageHandlerMock.AssertMessageAcceptedOnce(t, expectedMessage)
-}
-
-// TestServerShouldAcceptPOST1MessagesJsonWithEmptyMessage is a test function for the REST API call
-func TestServerShouldAcceptPOST1MessagesJsonWithEmptyMessage(t *testing.T) {
-
-	ImplTestServerShouldAcceptTLSPOST1MessagesJSON(t, 8502, "")
-}
-
-// TestServerShouldAcceptPOST1MessagesJsonWithMessage is a test function for the REST API call
-func TestServerShouldAcceptPOST1MessagesJsonWithMessage(t *testing.T) {
-
-	ImplTestServerShouldAcceptTLSPOST1MessagesJSON(t, 8503, "<dummy message>")
 }
