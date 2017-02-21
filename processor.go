68eea6b72589f8cf02d5989708cfda277ecbfa4d
Martin Jansa <martin.jansa@seznam.cz>
2017-02-21 23:51:11 +0100

    implemented conversion of the temporary errors to 202 (Accepted)

--- processor.go
+++ processor.go
@@ -21,6 +21,37 @@ func (p *Processor) HandleMessage(message PushNotification) (error, int) {
 	// simple forward of the received message to the Pushover connector and return the result
 	responseErr, reseponseCode := p.PushNotificationsSender.PostPushNotificationMessage(message)
 
+	acceptRequestToQueue := false
+
+	// if the call succeeded
+	if responseErr == nil {
+
+		// if the response represents a temporary error and we should enqueue the message and try later
+		switch reseponseCode {
+		case
+			504, // Gateway Timeout
+			598, // Network Read Timeout
+			599: // Network Timeout
+			acceptRequestToQueue = true
+		}
+
+	} else {
+
+		// if the posting failed we assume the sender works fine (should be checked by the production tests), but connection cannot be made temporarily
+		log.Printf("PushNotificationsSender.PostPushNotificationMessage failed with error %s.", responseErr.Error())
+
+		acceptRequestToQueue = true
+	}
+
+	// if the message should be accepted to queue
+	if acceptRequestToQueue {
+
+		// TODO: quing of the message and trying later should be done here!
+
+		// return HTTP error 202 (Accepted)
+		return nil, 202
+	}
+
 	return responseErr, reseponseCode
 }
 
